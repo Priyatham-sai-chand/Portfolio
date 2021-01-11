ada9bb373a9623a3c70f495fb8faa6f394f26771
Priyatham Sai Chand <priyathamsaichand@gmail.com>
2021-01-11 22:07:05 +0530

    intial project setup

--- pages/index.js
+++ pages/index.js
@@ -5,54 +5,67 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className= {styles.page}>
+        
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h2 className={styles.title}>Priyatham Sai Chand</h2>
+          <div className={styles.content}><div className={ styles.content__container }>
+            <p className={ styles.content__container__text }>
+              I'm am</p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <ul className={ styles.content__container__list }>
+              <li className={ styles.content__container__list__item }>a Programmer</li>
+              <li className={ styles.content__container__list__item }>a Full stack Dev</li>
+              <li className={ styles.content__container__list__item }>an Application Developer</li>
+              <li className={ styles.content__container__list__item }>a Techie</li>
+            </ul>
+            </div></div>
+      </main>
+    </div>
+            
+          
+        
+        <h2 className={styles.title}>Projects</h2>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://github.com/Priyatham-sai-chand/Locaft" className={styles.card}>
+            <h3>Locaft &rarr;</h3>
+            <p>A logistics and renting website for both owners and tenants alike</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://github.com/Priyatham-sai-chand/Steganography-in-C" className={styles.card}>
+            <h3>Steganography &rarr;</h3>
+            <p>Hiding images in plain bitmap images</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://github.com/Priyatham-sai-chand/Auction"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Auction &rarr;</h3>
+            <p>An Auctioning system for bidding items to the highest bidder using 
+              Django
+            </p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Alz. &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              A Mobile application to detect Alzheimers' prematurely.
             </p>
           </a>
         </div>
-      </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/Priyatham-sai-chand/alzapp"
           target="_blank"
           rel="noopener noreferrer"
         >
