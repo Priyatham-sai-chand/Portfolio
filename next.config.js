e2fa08b8a1e027083b01515100ef19d97a84894e
Priyatham Sai Chand <priyathamsaichand@gmail.com>
2021-01-11 23:35:11 +0530

    add statically cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    basePath: '/Portfolio',
-    assetPrefix: '/Portfolio/',
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/Priyatham-sai-chand/Portfolio/gh-pages/' : '',
 }
